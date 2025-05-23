285af658e4e2492e39c4f7949e066cb956b8c857
lucasoneves <casluhc@gmail.com>
2025-05-23 13:11:59 -0300

    Refatoração no container das paginas

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+
+  // app: {
+  //   pageTransition: { name: "page", mode: "in-out" },
+  // },
+  app: {
+    pageTransition: false,
+    layoutTransition: false,
+  },
+
+  vite: {
+    plugins: [tailwindcss(), tsconfigPaths()],
+  },
+
+  css: ["~/assets/css/main.css"],
+  modules: [
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Inter: [400, 700],
+        },
+      },
+    ],
+  ],
+});
---
+++ pages/challenges/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2 class="text-3xl font-bold">Challenges Page</h2>
+
+  <NuxtLink to="challenges/1">Challenge 1</NuxtLink>
+  <NuxtLink to="challenges/2">Challenge 2</NuxtLink>
+  <NuxtLink to="challenges/3">Challenge 3</NuxtLink>
+  <!-- 
+  <div v-if="data == null">No data</div>
+  <div v-for="item in data" :key="item.id">
+    {{ item.title }}
+  </div> -->
+</template>
+
+<script setup lang="ts">
+// const { data } = await useFetch("https://jsonplaceholder.typicode.com/posts/");
+
+// console.log(data.value);
+</script>
